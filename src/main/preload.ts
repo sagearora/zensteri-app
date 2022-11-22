53d94e60872e377b7246e8faf82e7e186d7f0a20
Saj Arora <saj.arora24@gmail.com>
2022-11-22 10:46:23 -0500

    fix preload

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,2 +1,26 @@
-// See the Electron documentation for details on how to use preload scripts:
-// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+import { contextBridge, ipcRenderer, IpcRendererEvent } from 'electron';
+
+export type Channels = 'zs-message';
+
+contextBridge.exposeInMainWorld('electron', {
+  ipcRenderer: {
+    invoke(channel: Channels, args: unknown[]) {
+      return ipcRenderer.invoke(channel, args)
+    },
+    sendMessage(channel: Channels, args: unknown[]) {
+      ipcRenderer.send(channel, args);
+    },
+    on(channel: Channels, func: (...args: unknown[]) => void) {
+      const subscription = (_event: IpcRendererEvent, ...args: unknown[]) =>
+        func(...args);
+      ipcRenderer.on(channel, subscription);
+
+      return () => {
+        ipcRenderer.removeListener(channel, subscription);
+      };
+    },
+    once(channel: Channels, func: (...args: unknown[]) => void) {
+      ipcRenderer.once(channel, (_event, ...args) => func(...args));
+    },
+  },
+});
